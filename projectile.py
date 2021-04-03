530ae78cb50bd0c9d46b3e40b828fe1e09acc643
Mad Dog <148394224+mcallan-star@users.noreply.github.com>
2021-04-02 21:30:19 -0400

    fixed the loop bound, the last point was dropping to zero

--- projectile.py
+++ projectile.py
@@ -13,21 +13,26 @@ dt = 0.01      #time step
 vx = v0 * np.cos(angle * np.pi/180)   #have to convert to radians, np.cos wants radians
 vy = v0 * np.sin(angle * np.pi/180)
 
-steps = 500
-x = np.zeros(steps)
-y = np.zeros(steps)
+# i dont know how many steps i need ahead of time so just stop when it hits the ground
+xs = [0.0]
+ys = [0.0]
+x, y = 0.0, 0.0
 
 # euler method (i think). update velocity then position every little dt
-for i in range(steps-1):       #stop one early so i dont go out of bounds
+# the bug before was my loop stopped one step early and left a fake (0,0) at the end.
+# now i just keep going until y goes negative, then keep that last point so the curve
+# actually reaches the ground
+while y >= 0 or len(ys) < 2:
     vy = vy - g*dt             #gravity only pulls down
-    x[i+1] = x[i] + vx*dt
-    y[i+1] = y[i] + vy*dt
+    x = x + vx*dt
+    y = y + vy*dt
+    xs.append(x)
+    ys.append(y)
 
-plt.plot(x, y)
+plt.plot(xs, ys)
 plt.xlabel("x (m)")
 plt.ylabel("y (m)")
 plt.title("projectile, no air")
 plt.grid(True)
 plt.axhline(0, color="black", linewidth=0.5)
-# why does it keep going below zero forever lol. and the very last point dips weird
 plt.show()
