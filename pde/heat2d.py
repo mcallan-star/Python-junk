ed33a3f4a4e2bd2dd53959e213c2f71689ee453b
Mad Dog <148394224+mcallan-star@users.noreply.github.com>
2023-09-24 21:48:17 -0400

    heat2d gif, watching it flow is so much better than two static frames

--- pde/heat2d.py
+++ pde/heat2d.py
@@ -1,16 +1,19 @@
-# 2d heat equation now. same idea but the laplacian uses all four neighbors:
+# 2d heat equation. laplacian uses all four neighbors:
 #   u_new = u + r*(up + down + left + right - 4*u)
-# stability is tighter in 2d, need r <= 0.25. start with a hot square and watch it
-# bleed out into a smooth blob. for now i just show the start and end frames, havent
-# learned to animate yet.
+# stability is tighter in 2d, need r <= 0.25. start with a couple hot squares and let
+# them bleed out and merge into a smooth blob.
+#
+# animated, watching the heat flow beats two static frames
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
 
 N = 120
 r = 0.2
 u = np.zeros((N, N))
-u[50:70, 50:70] = 1.0          #hot square
+u[25:45, 25:45] = 1.0
+u[70:95, 65:90] = 1.0          #two hot squares
 
 def step(u):
     un = u.copy()
@@ -18,13 +21,18 @@ def step(u):
         un[2:,1:-1] + un[:-2,1:-1] + un[1:-1,2:] + un[1:-1,:-2] - 4*un[1:-1,1:-1])
     return u
 
-start = u.copy()
-for _ in range(800):
-    u = step(u)
+fig, ax = plt.subplots(figsize=(5,5))
+im = ax.imshow(u, cmap="inferno", vmin=0, vmax=1)
+ax.axis("off"); ax.set_title("2d heat diffusion")
 
-fig, ax = plt.subplots(1, 2, figsize=(10,5))
-ax[0].imshow(start, cmap="inferno"); ax[0].set_title("start")
-ax[1].imshow(u, cmap="inferno"); ax[1].set_title("after 800 steps")
-for a in ax: a.axis("off")
-plt.suptitle("2d heat diffusion")
+steps_per_frame = 12
+def update(frame):
+    global u
+    for _ in range(steps_per_frame):
+        u = step(u)
+    im.set_array(u)
+    return [im]
+
+anim = FuncAnimation(fig, update, frames=80, blit=True, interval=50)
+anim.save("media/heat2d.gif", writer=PillowWriter(fps=20))
 plt.show()
